6dee841b3b60ad6a5f0eaa44269a867942d19edb
Antons Polskihs <antons.polskihs@gmail.com>
2025-02-02 21:05:36 +0200

    Initial Commit

---
+++ ArgosyApp/ArgosyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgosyAppApp.swift
+//  ArgosyApp
+//
+//  Created by Antons Polskihs on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArgosyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArgosyAppTests/ArgosyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArgosyAppTests.swift
+//  ArgosyAppTests
+//
+//  Created by Antons Polskihs on 02/02/2025.
+//
+
+import Testing
+@testable import ArgosyApp
+
+struct ArgosyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
